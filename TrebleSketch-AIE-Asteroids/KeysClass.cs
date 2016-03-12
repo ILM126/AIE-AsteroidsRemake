56acf3c477e4efb24bc7545f150f55c9ab3c6094
ILM126 <ILM126@hotmail.com.au>
2016-03-12 14:25:18 +1030

    Added Resolution and KeysClass

---
+++ TrebleSketch-AIE-Asteroids/KeysClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TrebleSketch_AIE_Asteroids
+{
+    class KeysClass
+    {
+        ShipClass Ship;
+        Game1 game;
+
+        public void ICheckINput(GameTime gameTime)
+        {
+            Ship.Acceleration = 0f;
+            Ship.RotationDelta = 0f;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                Ship.Acceleration = -0.08f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                Ship.Acceleration = 0.05f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                Ship.RotationDelta = -0.01f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                Ship.RotationDelta = 0.01f;
+            }
+            /*            if (Keyboard.GetState().IsKeyDown(Keys.Q)) // wants to slow vehicle down
+                        {
+                                Ship.Velocity = Ship.Velocity - new Vector2(-1, -1);
+                        } */
+            if (Keyboard.GetState().IsKeyDown(Keys.Q))
+            {
+                Ship.Velocity = new Vector2(0, 0);
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                game.ISpawnMISSle(gameTime);
+            }
+        }
+    }
+}
---
+++ TrebleSketch-AIE-Asteroids/Resolution.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TrebleSketch_AIE_Asteroids
+{
+    static class Resolution // Thanks to http://community.monogame.net/t/change-window-size-mid-game/1991
+    {
+        static public Matrix ScaleMatrix { get; set; }
+        static public Vector2 Scale { get; set; }
+        static public int GameWidth { get; set; }
+        static public int GameHeight { get; set; }
+    #if WINDOWS
+        static public int ScreenWidth { get; set; }
+        static public int ScreenHeight { get; set; }
+        static public Boolean WasResized { get; set; }
+        static private int PreviousWindowWidth;
+        static private int PreviousWindowHeight;
+    #endif
+
+
+        static public void Initialize(GraphicsDeviceManager graphics)
+        {
+        #if WINDOWS
+            ScreenWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+            ScreenHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+            PreviousWindowWidth = graphics.PreferredBackBufferWidth;
+            PreviousWindowHeight = graphics.PreferredBackBufferHeight;
+            WasResized = false;
+        #endif
+            GameWidth = 720;
+            GameHeight = 1080;
+            CalculateMatrix(graphics);
+        }
+
+
+        #if WINDOWS
+        static public void Update(Game Game1, GraphicsDeviceManager graphics)
+        {
+            if (WasResized)
+            {
+                if (graphics.PreferredBackBufferWidth < Resolution.GameWidth / 4)
+                {
+                    if (graphics.PreferredBackBufferWidth == 0) graphics.PreferredBackBufferWidth = PreviousWindowWidth;
+                    else graphics.PreferredBackBufferWidth = Resolution.GameWidth / 4;
+                }
+                if (graphics.PreferredBackBufferHeight < Resolution.GameHeight / 4)
+                {
+                    if (graphics.PreferredBackBufferHeight == 0) graphics.PreferredBackBufferHeight = PreviousWindowHeight;
+                    else graphics.PreferredBackBufferHeight = Resolution.GameHeight / 4;
+                }
+                graphics.ApplyChanges();
+                CalculateMatrix(graphics);
+                PreviousWindowWidth = graphics.PreferredBackBufferWidth;
+                PreviousWindowHeight = graphics.PreferredBackBufferHeight;
+                WasResized = false;
+            }
+        }
+        #endif
+
+        static void CalculateMatrix(GraphicsDeviceManager graphics)
+        {
+            ScaleMatrix = Matrix.CreateScale((float)graphics.PreferredBackBufferWidth / GameWidth, (float)graphics.PreferredBackBufferHeight / GameHeight, 1f);
+            Scale = new Vector2(ScaleMatrix.M11, ScaleMatrix.M22);
+        }
+    }
+}
