0fbe7eb281bfa55e62c2dabe6a9f632f75263298
ILM126 <ILM126@hotmail.com.au>
2016-03-14 18:04:49 +1030

    Tried to add AudioClass to handle audio

---
+++ TrebleSketch-AIE-Asteroids/AudioClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace TrebleSketch_AIE_Asteroids
+{
+    class AudioClass
+    {
+        /*public Song backgroundMusicIntro;
+        public Song backgroundMusicCore;
+        public Song backgroundMusicEnd;
+
+        public void ToggleMusic()
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.M))
+            {
+                MediaPlayer.Play(backgroundMusicIntro);
+                MediaPlayer.Play(backgroundMusicCore);
+                MediaPlayer.Play(backgroundMusicEnd);
+                
+            }
+        }*/
+    }
+}
