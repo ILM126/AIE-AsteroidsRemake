25f955c6f9408ded45a87f57e8c858b3495e1b71
ILM126 <ILM126@hotmail.com.au>
2016-05-19 00:15:54 +0930

    Last updates....

--- TrebleSketch-AIE-Asteroids/Properties/AssemblyInfo.cs
+++ TrebleSketch-AIE-Asteroids/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.5.0.1386")]
-[assembly: AssemblyFileVersion("0.1.0.185")]
+[assembly: AssemblyFileVersion("0.1.185.185")]
 [assembly: NeutralResourcesLanguage("en-AU")]
 
